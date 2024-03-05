69abe996c5bf877404af32c99eb9cdfa372242d9
danimisme <msubhanr53@gmail.com>
2024-03-05 22:18:08 +0700

    use imperative routing

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,9 +1,15 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const LoginPage = () => {
+  const router = useRouter();
+  const handleLogin = () => {
+    router.push("/product");
+  };
   return (
     <div>
       <h1>Login Page</h1>
+      <button onClick={handleLogin}>Login</button>
       Belum punya akun? <Link href="/auth/register">Register</Link>
     </div>
   );
