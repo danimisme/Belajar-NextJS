a8dc55d4809ef4d54b3fd842e1cb5cb6c9443568
danimisme <msubhanr53@gmail.com>
2024-03-06 23:03:04 +0700

    installing tailwind css for styling

--- src/views/Auth/Login/index.tsx
+++ src/views/Auth/Login/index.tsx
@@ -9,7 +9,7 @@ const LoginViews = () => {
   };
   return (
     <div className={styles.login}>
-      <h1 className="big">Login Page</h1>
+      <h1 className="text-3xl ">Login Page</h1>
       <button onClick={handleLogin}>Login</button>
       <p style={{ color: "red", border: "1px solid red", borderRadius: "5px" }}>
         Belum punya akun? <Link href="/auth/register">Register</Link>
