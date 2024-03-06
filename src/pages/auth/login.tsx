72b5dd17a577ce85001267f929cb0f7313fd725b
danimisme <msubhanr53@gmail.com>
2024-03-06 22:35:02 +0700

    Refactoring LoginPage

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,21 +1,10 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-import styles from "./login.module.css";
+import LoginViews from "@/views/Auth/Login";
 
 const LoginPage = () => {
-  const { push } = useRouter();
-  const handleLogin = () => {
-    push("/product");
-  };
   return (
-    <div className={styles.login}>
-      <h1>Login Page</h1>
-      <button onClick={handleLogin}>Login</button>
-      <p>
-        {" "}
-        Belum punya akun? <Link href="/auth/register">Register</Link>{" "}
-      </p>
-    </div>
+    <>
+      <LoginViews />
+    </>
   );
 };
 
---
+++ src/views/Auth/Login/index.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from "./login.module.css";
+
+const LoginViews = () => {
+  const { push } = useRouter();
+  const handleLogin = () => {
+    push("/product");
+  };
+  return (
+    <div className={styles.login}>
+      <h1>Login Page</h1>
+      <button onClick={handleLogin}>Login</button>
+      <p>
+        {" "}
+        Belum punya akun? <Link href="/auth/register">Register</Link>{" "}
+      </p>
+    </div>
+  );
+};
+export default LoginViews;
