c873b21182c8544b617c0abde961728ddde2927b
danimisme <msubhanr53@gmail.com>
2024-03-13 22:25:12 +0700

    learn how to using database (firestore) and  create API

--- src/pages/api/product.ts
+++ src/pages/api/product.ts
@@ -1,32 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { retrieveData } from '@/lib/firebase/service';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
     status: boolean,
     statusCode: number,
-  data: {
-    id: number
-    name: string
-    price: number
-    size: string
-  }[];
+  data: any;
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-    const data = [{
-        id:1,
-        name: 'Baju Baru',
-        price : 500000,
-        size: "XL"
-    },
-    {
-        id:2,
-        name: 'Baju Lama',
-        price : 100000,
-        size: "L"
-    }]
+  const data = await retrieveData("products")
   res.status(200).json({ status : true, statusCode: 200,data})
 }
