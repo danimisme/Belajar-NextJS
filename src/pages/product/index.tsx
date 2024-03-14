696ac5f2a1e4f2bcc3352dc3efa4e3d94fcebd15
danimisme <msubhanr53@gmail.com>
2024-03-14 22:16:27 +0700

    learn CSR (Client Side Rendering), add skeleton when data is not ready

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,16 +1,12 @@
+import ProductView from "@/views/Product";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 
-type productType = {
-  id: number;
-  name: string;
-  price: number;
-  size: string;
-};
 const ProductPage = () => {
   const [isLogin, setIsLogin] = useState(true);
   const [products, setProducts] = useState([]);
   const { push } = useRouter();
+
   useEffect(() => {
     if (!isLogin) {
       push("/auth/login");
@@ -21,14 +17,11 @@ const ProductPage = () => {
     fetch("/api/product")
       .then((res) => res.json())
       .then((response) => setProducts(response.data));
-  });
+  }, []);
 
   return (
     <div>
-      <h1>Product Page</h1>
-      {products.map((product: productType) => (
-        <p key={product.id}>{product.name}</p>
-      ))}
+      <ProductView products={products} />
     </div>
   );
 };
