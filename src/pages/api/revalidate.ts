4bdae1e7665821ce27c346267adf374dc36722f2
danimisme <msubhanr53@gmail.com>
2024-03-15 23:11:48 +0700

    add triger to revalidate (static site regeneration)

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -2,17 +2,25 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  revalidate: boolean
+  revalidated: boolean
+  message?: string
 }
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  try {
-    await res.revalidate('/product/static')
-    return res.json({revalidate: true})
-  } catch (error) {
-    return res.status(500).send({revalidate: false})
-  }
+    if (req.query.token !== process.env.REVALIDATE_TOKEN) {
+        return res.status(401).json({ revalidated: false, message: 'Invalid token' })
+    }
+    if (req.query.data === "product") { 
+        try {
+            await res.revalidate('/product/static')
+            return res.json({revalidated: true})
+          } catch (error) {
+            return res.status(500).send({revalidated: false})
+          }
+    }
+    return res.json({revalidated: false , message: "select your data first"})
+
 }
