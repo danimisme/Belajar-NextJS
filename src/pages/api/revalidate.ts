ed7881abbd3b5ba8b9b48ce1ec6d6c728d4b165f
danimisme <msubhanr53@gmail.com>
2024-03-15 22:30:53 +0700

    incremental static regeneration

---
+++ src/pages/api/revalidate.ts
@@ -0,0 +1,18 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  revalidate: boolean
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  try {
+    await res.revalidate('/product/static')
+    return res.json({revalidate: true})
+  } catch (error) {
+    return res.status(500).send({revalidate: false})
+  }
+}
