ed9170a6bd746f6e407e3cb6ec5fe4ee338fff5d
danimisme <msubhanr53@gmail.com>
2024-03-15 23:27:04 +0700

    learn how to use middle ware

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -5,23 +5,10 @@ import useSWR from "swr";
 import { fetcher } from "@/lib/swr/fetcher";
 
 const ProductPage = () => {
-  const [isLogin, setIsLogin] = useState(true);
   const [products, setProducts] = useState([]);
   const { push } = useRouter();
 
-  useEffect(() => {
-    if (!isLogin) {
-      push("/auth/login");
-    }
-  }, []);
-
   const { data, error, isLoading } = useSWR("/api/product", fetcher);
-
-  // useEffect(() => {
-  //   fetch("/api/product")
-  //     .then((res) => res.json())
-  //     .then((response) => setProducts(response.data));
-  // }, []);
 
   return (
     <div>
