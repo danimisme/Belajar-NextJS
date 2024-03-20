ff3b750a5708028d095c6f14adaa3f3a0b4378f2
danimisme <msubhanr53@gmail.com>
2024-03-20 22:00:30 +0700

    font Optimization

--- src/components/layouts/AppShell/index.tsx
+++ src/components/layouts/AppShell/index.tsx
@@ -1,16 +1,21 @@
 import { useRouter } from "next/router";
 import Navbar from "../Navbar";
-
+import { Roboto } from "next/font/google";
 type AppShellProps = {
   children: React.ReactNode;
 };
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 const disableNavbar = ["/auth/login", "/auth/register", "/404"];
 const AppShell = (props: AppShellProps) => {
   const { children } = props;
   const { pathname } = useRouter();
   console.log(pathname);
   return (
-    <main>
+    <main className={roboto.className}>
       {!disableNavbar.includes(pathname) && <Navbar />}
       {children}
     </main>
